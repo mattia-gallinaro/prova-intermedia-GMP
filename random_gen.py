53ddc6ea1e57133b6f26b4c1d7e8eabdd76b5a32
mattia-gallinaro <72882041+mattia-gallinaro@users.noreply.github.com>
2024-11-21 22:18:50 +0100

    Dovrebbe generare numeri random in un txt , così possiamo fare la lettura da txt
    Basta fare un stream in input che legge dal file

--- random_gen.py
+++ random_gen.py
@@ -1,3 +1,4 @@
 import random
+f = open('numbers_gen.txt', "a")
 for i in range(181)
-     print(random.uniform(0, 2))#ritorna un float tra 0 e 2 
+     f.write(random.uniform(0, 2))#ritorna un float tra 0 e 2 
