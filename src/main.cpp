1e2e8c52b7d0cf91d8c8132928f6b6aa812dd67a
mattia-gallinaro <72882041+mattia-gallinaro@users.noreply.github.com>
2024-11-24 14:15:41 +0100

    funzione per generare numeri random

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <fstream>
+#include <random>
+
 using namespace std;
 
 
 void read_from_file(void);
-
+void test_random(void);
 int main(void){}
 
 void read_from_file(void){
@@ -14,4 +16,15 @@ void read_from_file(void){
     cout << text;
   }
   MyFile.close();
+}
+
+void test_random(){
+
+  int lower = 0;
+  int upper = 2;
+  std::default_random_engine rnd{std::random_device{}()};//permetter di generare i numeri random 
+  std::uniform_real_distribution<double> dist(lower, upper);//per far si che ogni numero abbia la stessa probabilità di uscire
+
+  for(int i = 0; i < 10; i++)std::cout << dist(rnd) << std::endl; ;//genera un numero random compreso nella distribuzione uniforme ,
+                                                                   // quindi tra upper e lower
 }
